0d47cc088edbb28974c519227947468ed1d0dbc3
tanjoshty <tanjoshty@gmail.com>
2020-10-31 08:47:22 +1100

    implemented redux

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,9 +8,9 @@ const connectDB = async () => {
             useCreateIndex: true
         })
 
-        console.log(`mongodb connected: ${conn.connection.host}`)
+        console.log(`mongodb connected: ${conn.connection.host}`.cyan.underline)
     } catch (error) {
-        console.error(`Error: ${error.message}`)
+        console.error(`Error: ${error.message}`.red.underline.bold)
         process.exit(1)
     }
 }
