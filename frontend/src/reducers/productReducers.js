a8d87bcf4aabb85cfb61b2bfde52d49f0898fe52
tanjoshty <tanjoshty@gmail.com>
2020-11-09 12:56:24 +1100

    pagination, search and review function added

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,11 +1,11 @@
-import {PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL, PRODUCT_DELETE_FAIL, PRODUCT_DELETE_REQUEST, PRODUCT_DELETE_SUCCESS, PRODUCT_CREATE_REQUEST, PRODUCT_CREATE_SUCCESS, PRODUCT_CREATE_FAIL, PRODUCT_CREATE_RESET, PRODUCT_UPDATE_REQUEST, PRODUCT_UPDATE_SUCCESS, PRODUCT_UPDATE_FAIL, PRODUCT_UPDATE_RESET} from '../constants/productConstants'
+import {PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL, PRODUCT_DELETE_FAIL, PRODUCT_DELETE_REQUEST, PRODUCT_DELETE_SUCCESS, PRODUCT_CREATE_REQUEST, PRODUCT_CREATE_SUCCESS, PRODUCT_CREATE_FAIL, PRODUCT_CREATE_RESET, PRODUCT_UPDATE_REQUEST, PRODUCT_UPDATE_SUCCESS, PRODUCT_UPDATE_FAIL, PRODUCT_UPDATE_RESET, PRODUCT_CREATE_REVIEW_FAIL, PRODUCT_CREATE_REVIEW_REQUEST, PRODUCT_CREATE_REVIEW_RESET, PRODUCT_CREATE_REVIEW_SUCCESS} from '../constants/productConstants'
 
 export const productListReducer = (state = {products: []}, action) => {
     switch(action.type) {
         case PRODUCT_LIST_REQUEST:
             return { loading: true, products: [] }
         case PRODUCT_LIST_SUCCESS:
-            return { loading: false, products: action.payload }
+            return { loading: false, products: action.payload.products, pages: action.payload.pages, page: action.payload.page }
         case PRODUCT_LIST_FAIL:
             return { loading: false, error:action.payload }
         default:
@@ -68,6 +68,22 @@ export const productUpdateReducer = (state = {product: {}}, action) => {
             return { loading: false, error: action.payload }
         case PRODUCT_UPDATE_RESET:
             return {product: {}}
+        default:
+            return state
+
+    }
+}
+
+export const productReviewCreateReducer = (state = {}, action) => {
+    switch(action.type) {
+        case PRODUCT_CREATE_REVIEW_REQUEST:
+            return { loading: true }
+        case PRODUCT_CREATE_REVIEW_SUCCESS:
+            return { loading: false, success: true }
+        case PRODUCT_CREATE_REVIEW_FAIL:
+            return { loading: false, error: action.payload }
+        case PRODUCT_CREATE_REVIEW_RESET:
+            return {}
         default:
             return state
 
